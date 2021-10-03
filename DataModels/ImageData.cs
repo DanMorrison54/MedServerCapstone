f71ec6e249cc6a722765f2c1e94d779f20d60bf1
Danmorris3214 <danmorris3214@gmail.com>
2021-10-03 14:24:17 -0400

    commiting models

--- DataModels/ImageData.cs
+++ DataModels/ImageData.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ML.Data;
 
-namespace MedServerCapstone.MLModels
+namespace MedServerCapstone.DataModels
+
 {
-    public class Class
+    public class ImageData
     {
+        [ColumnName("Label"), LoadColumn(0)]
+        public string Label { get; set; }
+
+
+        [ColumnName("ImageSource"), LoadColumn(1)]
+        public string ImageSource { get; set; }
     }
 }
--- DataModels/ImageOutput.cs
+++ DataModels/ImageOutput.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ML.Data;
 
 namespace MedServerCapstone.DataModels
 {
-    public class ImageOutput
+    public class ImageOutput: ImageData
     {
+        // ColumnName attribute is used to change the column name from
+        // its default value, which is the name of the field.
+        [ColumnName("PredictedLabel")]
+        public String Prediction { get; set; }
+        public float[] Score { get; set; }
     }
 }
